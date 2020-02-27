ca07f6d95e561d77b111bfe8d41696d567215630
mohammed <mdrame113@gmail.com>
2020-02-27 11:16:36 -0800

    tableView popurlated

--- Hebitual/Hebitual/ViewControllers /HabitsTableViewController.swift
+++ Hebitual/Hebitual/ViewControllers /HabitsTableViewController.swift
@@ -8,23 +8,27 @@
 
 import UIKit
 
-class HabitsTableViewController: UIViewController {
+class HabitsTableViewController: UITableViewController {
+    var names: [String] = ["Alan", "Braus", "Adriana", "Mitchell", "Dani", "Jess", "Dan", "Meredith", "Dan", "Milad"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // TableView Delegate and DataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        if let dequeueCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = dequeueCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
 }
